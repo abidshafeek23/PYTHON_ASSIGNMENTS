98d2ae3b1109fecf40576f1d66e43bfe9d4821fb
abidshafeek23 <funoonwork@gmail.com>
2025-01-22 14:13:00 +0530

    Add files via upload

---
+++ PYTHON_ASSIGNMENT_2.py
@@ -0,0 +1,32 @@
+"""
+random_numbers = [7, 12, 19, 4, 23]
+print("List of random numbers:", random_numbers)
+
+random_numbers.extend([258, 715, 930])
+print("List new :", random_numbers)
+
+for number in random_numbers:
+    print("Element in the list:", number)
+
+person = {'name': 'ali khan', 'age': 25, 'address': 'kollam'}
+print("Dictionary:", person)
+
+person['phone'] = '9777585428'
+print("Updated dictionary:", person)
+
+numbers_set = {1, 2, 3, 4, 5}
+print("Set:", numbers_set)
+
+numbers_set.discard(3)
+print("Set after removing 3:", numbers_set)
+
+numbers_set.add(6)
+print("Set after adding 6:", numbers_set)
+
+"""
+numbers_tuple = (1, 2, 3, 4)
+print("Tuple:", numbers_tuple)
+
+print("Length of the tuple:", len(numbers_tuple))
+
+
